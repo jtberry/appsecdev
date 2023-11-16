6f87ef4597d03feae23c4096e73f71ab561c8f8a
jtberry <selflessjason@gmail.com>
2023-11-16 10:00:42 -0700

    fixed typo

--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -2,7 +2,7 @@ provider "aws"{
     region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "terra-state"{
+resource "aws_s3_bucket" "terra_state"{
     bucket = "lock-state-for-terra"
     lifecycle {
      prevent_destroy = true
